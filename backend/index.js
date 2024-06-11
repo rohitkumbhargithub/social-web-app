a86b23ed1cc624f60f60e89e2b618a8145182185
rohitkumbhargithub <rohitkumbhar956@gmail.com>
2024-06-11 19:07:11 +0530

    deploy

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+const __variableOfChoice = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 // limit shouldn't be too high to prevent DOS
@@ -35,10 +35,10 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__variableOfChoice,"/frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.join(__variableOfChoice, "frontend", "dist", "index.html"));
 	});
 }
 
